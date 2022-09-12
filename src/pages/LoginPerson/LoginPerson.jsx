87489e51766b663045763b07cab9ad98b88cc88c
vpugliese <vpugliese@pall.cl>
2022-09-12 09:34:41 -0300

    Update: integrating react-simple-oauth2-login

--- src/pages/LoginPerson/LoginPerson.jsx
+++ src/pages/LoginPerson/LoginPerson.jsx
@@ -7,6 +7,11 @@ import '../../styles/Transitions.scss'
 import useAuth from '../../hooks/useAuth';
 import Loader from "../../components/Loader";
 // import { loginServiceFetch } from "../../services/loginService";
+import OAuth2Login from 'react-simple-oauth2-login';
+import logo from '../../assets/statics/logo-light.svg'
+import logoTGD from '../../assets/statics/logo-tgd-colors.svg'
+import { get } from "../../services/httpServices";
+import { useCallback } from "react";
 
 
 function LoginPerson() {
@@ -22,16 +27,19 @@ function LoginPerson() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [number, setNumber] = useState(2) // Background
 
+    const onSuccess = response => console.log(response);
+    const onFailure = response => console.error(response);
+
     useEffect(() => {
         if (auth.isLogged()) history.push(previousObjetURL || "/usuario")
     }, [auth, history, previousObjetURL])
 
     const onSubmit = () => {
         // setLoading(true)
-        auth.loginPerson(email, password);
+        auth.loginPerson('violeta.pugliese@gmail.com', 123546);
         if (saveData) {
-            localStorage.setItem("loginDataEmail", JSON.stringify(email));
-            localStorage.setItem("loginDataPassword", JSON.stringify(password));
+            localStorage.setItem("loginDataEmail", JSON.stringify('violeta.pugliese@gmail.com'));
+            localStorage.setItem("loginDataPassword", JSON.stringify(123546));
         }
     }
 
@@ -44,69 +52,26 @@ function LoginPerson() {
             {loading
                 ? <Loader isActive={loading} />
                 : <div className={`bg-container bg${number} `}>
-                    <Container className='z-index-1 cont'>
-                        <Row className='w-100'>
-                            <Col xs={12} className="d-flex flex-column justify-content-around h-100">
-                                <h2>Iniciar sesión</h2>
-                                <Form className="form-group in" onSubmit={handleSubmit(onSubmit)}>
-                                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Label>Email</Form.Label>
-                                        <Form.Control
-                                            name="email"
-                                            type="text"
-                                            value={email}
-                                            className="form-control"
-                                            {...register('email', {
-                                                required: {
-                                                    value: true,
-                                                    message: "El campo es requerido."
-                                                },
-                                                pattern: {
-                                                    value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-                                                    message: "El formato ingresado no es válido"
-                                                }
-                                            })}
-                                            onChange={(e) => { setEmail(e.target.value) }}
-                                        />
-                                        {errors.email && <ErrorMessage><p>{errors.email.message}</p></ErrorMessage>}
-                                    </Form.Group>
-                                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                                        <Form.Label>Password</Form.Label>
-                                        <Form.Control
-                                            name="password"
-                                            type="password"
-                                            value={password}
-                                            className="form-control"
-                                            {...register('password', {
-                                                required: {
-                                                    value: true,
-                                                    message: "El campo es requerido."
-                                                },
-                                                minLength: {
-                                                    value: 3,
-                                                    message: "La contraseña debe tener al menos 3 caracteres",
-                                                }
-                                            })}
-                                            onChange={(e) => { setPassword(e.target.value) }}
-                                        />
-                                        {errors.password && <ErrorMessage><p>{errors.password.message}</p></ErrorMessage>}
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Label><input type="checkbox" onChange={(e) => { setSaveData(e.target.value) }} /> Recordar usuario y contraseña</Form.Label>
-                                    </Form.Group>
-                                    <Form.Group>
-                                        <Form.Label>
-                                            <Link to="/recuperar-clave">¿Olvidaste tu contraseña?</Link>
-                                        </Form.Label>
-                                    </Form.Group>
-                                    <div className="d-flex flex-column align-items-center align-items-sm-end mt-3">
-                                        <Button variant="primary" type="submit">
-                                            Iniciar Sesión
-                                        </Button>
-                                        <button type="button" className="btn btn-ligth mt-3" >
-                                            <Link to="/register" className="text-primary">Crear cuenta</Link>
-                                        </button>
-                                    </div>
+                    <Container className='z-index-1 '>
+                        <Row className='w-100 d-flex justify-content-center in'>
+                            <Col xs={10} md={4} lg={3} className="d-flex flex-column justify-content-around align-items-center h-100">
+                                <img src={logo} className="w-100" alt="Logo blanco Portal del paciente Chaco" />
+                                <h6 className="text-center text-light my-3">Ingresar con: </h6>
+                                <OAuth2Login
+                                    className="btn-tgd"
+                                    authorizationUrl="http://stage.ventanillaunica.chaco.gov.ar/oauth/v2/auth"
+                                    responseType="code"
+                                    clientId="109_469fzlhy0084gkscg4gsk8k88ow4kgggso8s44ososo80ccos8"
+                                    redirectUri="http://test-portal.salud.chaco.gob.ar/callback"
+                                    isCrossOrigin={true}
+                                    onSuccess={onSuccess}
+                                    onFailure={onFailure}>
+                                        <img src={logoTGD} className="w-75 my-4" alt="Logo Tu Gobierno Digital" />
+                                    </OAuth2Login>
+                                <Form className="form-group in mt-5" onSubmit={handleSubmit(onSubmit)}>
+                                    <Button variant="primary" type="submit">
+                                        Iniciar Sesión
+                                    </Button>
                                 </Form>
                             </Col>
                         </Row>
