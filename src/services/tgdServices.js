7b9bf30893a313842079c93f37b1327cb3b049f5
violetapugliese <violeta.pugliese@gmail.com>
2022-09-15 12:16:43 -0300

    Update: activating oauth flow

--- src/services/tgdServices.js
+++ src/services/tgdServices.js
@@ -5,20 +5,20 @@ export default async function tgdServiceToken(params) {
     try {
         const searchParams = new URLSearchParams(params);
         let query = searchParams.toString();
-        // const promise = await get(TGD_GET_USER_TOKEN(query))
-        // return promise
+        const promise = await get(TGD_GET_USER_TOKEN(query))
+        return promise
     }
     catch (err) {
-        console.log('Error al cargar Sumar: ', err);
+        console.error('Error al solicitar token TGD: ', err);
     }
 }
 
 export async function tgdServiceUserData(tgdToken) {
     try {
-        // const promise = await get(TGD_GET_USER_DATA, TGD_HEADER(tgdToken))
-        // return promise
+        const promise = await get(TGD_GET_USER_DATA(), TGD_HEADER(tgdToken))
+        return promise
     }
     catch (err) {
-        console.log('Error al cargar Sumar: ', err);
+        console.error('Error al solicitar datos de persona TGD: ', err);
     }
 }
