9b3e09b09fbed96fb707d56de2e062cc99a49bc6
violetapugliese <violeta.pugliese@gmail.com>
2022-11-08 11:40:40 -0300

    update: adding btn

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 import { Container, Row, Col, } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
 
 function Register() {
@@ -8,14 +9,19 @@ function Register() {
 
     return (
         <div className={`bg-container bg${number}`}>
-                <Container className='z-index-1 cont'>
-                    <Row className='w-100'>
-                        <Col xs={12} className="d-flex flex-column h-100 justify-content-between pb-3 pb-sm-0">
-                            <h2 className='d-flex'>Registrarse</h2>
-                                <RegisterForm formType={"user"} ></RegisterForm>
-                        </Col>
-                    </Row>
-                </Container>
+            <Container className='z-index-1 cont d-flex flex-column'>
+                <Row className='w-100'>
+                    <Col xs={12} className="d-flex flex-column h-100 justify-content-between pb-3 pb-sm-0">
+                        <h2 className='d-flex'>Registrarse</h2>
+                        <RegisterForm formType={"user"} ></RegisterForm>
+                    </Col>
+                </Row>
+                <Row className="w-100">
+                    <Col className="d-flex align-items-end justify-content-end">
+                        <Link to="/login" className='text-primary mt-2'>ir a Iniciar sesión</Link>
+                    </Col>
+                </Row>
+            </Container>
         </div>
 
     )
