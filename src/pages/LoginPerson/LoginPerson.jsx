4eb2aff359ac569a8d7eb8c63edf5cae96ff6704
violetapugliese <violeta.pugliese@gmail.com>
2023-03-17 21:01:40 -0300

    Update: cleaning

--- src/pages/LoginPerson/LoginPerson.jsx
+++ src/pages/LoginPerson/LoginPerson.jsx
@@ -29,8 +29,6 @@ function LoginPerson() {
     const location = useLocation();
     // LEVANTA DATOS DESDE TGD
     const data = location.search
-    // login?id=5&surname=NUñEZ&name=FERNANDO&identification_number=26806376&birthdate=None&id_gender=None&id_department=None&id_locality=None&address_street=None&address_number=None&id_usual_institution=None&is_diabetic=None&is_hypertensive=None&is_chronic_respiratory_disease=None&is_chronic_kidney_disease=None&identification_number_master=None&id_identification_type=None&id_identification_type_master=None&is_deleted=None&id_patient=None&id_admin_status=None&phone_number=None&department=None&locality=None&email=None&identification_front_image=None&identification_back_image=None&identification_front_image_file_type=None&identification_back_image_file_type=None&id_person_status=None&is_admin=0&access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyNjgwNjM3NiIsImV4cCI6MTY3ODc1MzQxNn0.gjj7M438vJ17QgUqLkr7nSKFHAwScuOXMvQVG1H4PMs
-    // const url = ('id_person=3&access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvb2NhbnRvIiwiZXhwIjoxNjc1Nzk2NTE4fQ.YCCYMM1m0ApL2Q_Qs6ZZzCG7oba3vbQixLlWlFW6z84&token_type=bearer&data=%7B%27id%27%3A+3%2C+%27surname%27%3A+%27Ocanto%27%2C+%27name%27%3A+%27Osvaldo%27%2C+%27identification_number%27%3A+%2727543642%27%2C+%27birthdate%27%3A+%271979-01-01%27%2C+%27id_person_status%27%3A+2%2C+%27family_group%27%3A+%5B%5D%7D')
     const previousObjetURL = location.state?.from
 
     const tgdCredentials = {
