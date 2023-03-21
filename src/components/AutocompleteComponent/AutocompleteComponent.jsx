a129368d9fdbe7b92938b8bae5948547d3438382
violetapugliese <violeta.pugliese@gmail.com>
2023-03-21 10:19:21 -0300

    Fix: autocomplete efectores

--- src/components/AutocompleteComponent/AutocompleteComponent.jsx
+++ src/components/AutocompleteComponent/AutocompleteComponent.jsx
@@ -6,74 +6,74 @@ import { ReactSearchAutocomplete } from 'react-search-autocomplete';
 
 const AutocompleteComponent = React.forwardRef((props, ref) => {
 
-    const { variants, name, handleChange, onBlur, selectValue, disabled } = props;
-    const [idValue, setIdValue] = useState(selectValue ? selectValue : false)
-    const [items, setItems] = useState([]);
-    const [value, setValue] = useState('');
+  const { variants, name, handleChange, onBlur, selectValue, disabled } = props;
+  const [idValue, setIdValue] = useState(selectValue ? selectValue : false);
+  const [items, setItems] = useState([]);
+  const [value, setValue] = useState('');
 
+  useEffect(() => {
+    if (variants) {
+      let items = variants.map((item) => {
+        return { id: item.cuie, name: item.Servicio.split(' - ')[1] }
+      });
+      setItems(items);
+    }
+  }, [variants]);
 
-    useEffect(() => {
-        if (variants) {
-           let items = variants.map((item) => {
-            return {id: item.cuie, name: item.Servicio.split(' - ')[1]}
-            });
-            setItems(items);
-        }
+  const handleOnSearch = (string, results) => {
+    handleChange(string)
+  }
 
-    }, [variants])
+  const handleOnHover = (result) => {
+  }
 
-    const handleOnSearch = (string, results) => {
-      handleChange(string)
+  const handleOnSelect = (item) => {
+    if (item && item.id) {
+      handleChange(item)
+      setIdValue(item.id)
+    }
+  }
+
+  const handleOnFocus = () => {
+    // setItems(items);
+  }
+
+  const formatResult = (item) => {
+  }
+
+  useEffect(() => {
+    //SETEA EL VALOR COMO PLACEHOLDER EN INPUT Y TMB SETEA EL VALOR EN EL FORMULARIO. 
+    // LA LIBRERIA react-search-autocomplete NO PERMITE SETEAR UN VALOR POR DEFECTO EN EL INPUT
+    if (idValue) {
+      let item = items.find((ins) => ins.id === idValue);
+      if (item) {
+        handleOnSelect(item);
+        setValue(item.name)
       }
-    
-      const handleOnHover = (result) => {
-      }
-    
-      const handleOnSelect = (item) => {
-        if (item && item.id) {
-          handleChange(item)
-          setIdValue(item.id)
-        }
-      }
-    
-      const handleOnFocus = () => {
-        // setItems(items);
-      }
-
-      const formatResult = (item) => {
-      }
-
-      useEffect(() => {
-        //SETEA EL VALOR COMO PLACEHOLDER EN INPUT Y TMB SETEA EL VALOR EN EL FORMULARIO. 
-        // LA LIBRERIA react-search-autocomplete NO PERMITE SETEAR UN VALOR POR DEFECTO EN EL INPUT
-        if (idValue) {
-          let item = items.find((ins) => ins.id === idValue);
-          if (item) {
-            handleOnSelect(item);
-            setValue(item.name)
-          }
-        }
-      }, [idValue, items])
+    }
+  }, [idValue, items])
 
 
-    return (
-        <ReactSearchAutocomplete
-            items={items}
-            onSelect={handleOnSelect}
-            onSearch={handleOnSearch}
-            styling={{
-                height: "34px",
-                border: "1px solid gray",
-                borderRadius: "4px",
-                backgroundColor: "white",
-                boxShadow: "none",
-                fontSize: "12px",
-                fontFamily: "Courier",
-                clearIconMargin: "3px 8px 0 0",
-                zIndex: 2,
-              }}
-            placeholder='Buscar...'
-        />
-    )
+  return (
+    <ReactSearchAutocomplete
+      items={items}
+      onSelect={handleOnSelect}
+      onSearch={handleOnSearch}
+      styling={{
+        height: "34px",
+        border: "1px solid gray",
+        borderRadius: "4px",
+        backgroundColor: "white",
+        boxShadow: "none",
+        fontSize: "12px",
+        fontFamily: "Courier",
+        clearIconMargin: "3px 8px 0 0",
+        zIndex: 2,
+      }}
+      placeholder={value || 'Buscar...'}
+      autoFocus={false}
+      showItemsOnFocus={true}
+    />
+  )
 })
 export default AutocompleteComponent;
